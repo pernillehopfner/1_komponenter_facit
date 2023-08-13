d97ac72eab4899be42a2197cf1cb55b8148594f8
pelolsen <pelolsen@hotmail.com>
2023-08-13 16:24:44 +0200

    Updated

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+/*Mine brugte komponenter*/
+import FirstComponent from "./components/FirstComponent";
+import PropsComponent from "./components/PropsComponent";
+import ButtonComponent from "./components/ButtonComponent";
+import InputComponent from "./components/InputComponent";
+import AssetComponent from "./components/AssetComponent";
+
+/*Mit billede*/
+import FirstImage from "./assets/favicon.png"
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Komponent uden nogen props*/}
+      <FirstComponent/>
+
+      {/*Komponent med Props*/}
+      <PropsComponent name={"Eigil & jesper"}/>
+
+      {/* Dynamisk farveknap */}
+      <ButtonComponent/>
+
+      {/*Input text felt*/}
+      <InputComponent/>
+
+      {/*Billede component med en prop kaldt url, som bliver passet til min Asset Compoennt*/}
+      <AssetComponent url={FirstImage}/>
+    
     </View>
   );
 }
